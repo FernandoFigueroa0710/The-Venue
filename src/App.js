5f7c69c7bb7ff4b4a7ffb413374d5dbf0af08e5c
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-15 14:45:19 -0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import "./resources/styles.css";
 
+import Header from "../src/components/header_footer/Header";
+
 class App extends Component {
   render() {
-    return <div>Hello world.....</div>;
+    return <Header />;
   }
 }
 
