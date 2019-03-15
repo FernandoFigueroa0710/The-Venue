49a74d5fb6bcf5dbc6b8f301e4785e182dab8a1e
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-15 15:19:50 -0600

    button created with event listener

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import MenuIcon from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
 import IconButton from "@material-ui/core/IconButton";
 
 class Header extends Component {
@@ -21,6 +21,9 @@ class Header extends Component {
             <div className="font_righteous header_logo_venue">The Venue</div>
             <div className="header_logo_title">Musical Events</div>
           </div>
+          <IconButton onClick={() => console.log("open")}>
+            <MenuIcon aria-label="menu" color="secondary" />
+          </IconButton>
         </Toolbar>
       </AppBar>
     );
