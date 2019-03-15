b2e2d3dd52e13f6bbed5a56a6b02ebe73c136dd2
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-15 16:45:26 -0600

    add toggel functions to side drawer

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -5,7 +5,18 @@ import Toolbar from "@material-ui/core/Toolbar";
 import MenuIcon from "@material-ui/icons/Menu";
 import IconButton from "@material-ui/core/IconButton";
 
+import SideDrawer from "./SideDrawer";
+
 class Header extends Component {
+  state = {
+    drawerOpen: false
+  };
+
+  toggleDrawer = value => {
+    this.setState({
+      drawerOpen: value
+    });
+  };
   render() {
     return (
       <AppBar
@@ -21,9 +32,13 @@ class Header extends Component {
             <div className="font_righteous header_logo_venue">The Venue</div>
             <div className="header_logo_title">Musical Events</div>
           </div>
-          <IconButton onClick={() => console.log("open")}>
+          <IconButton onClick={() => this.toggleDrawer(true)}>
             <MenuIcon aria-label="menu" color="secondary" />
           </IconButton>
+          <SideDrawer
+            open={this.state.drawerOpen}
+            onClose={value => this.toggleDrawer(value)}
+          />
         </Toolbar>
       </AppBar>
     );
