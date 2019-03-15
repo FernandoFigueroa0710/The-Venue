e4b6473ba422891cef62ea8fe9eb7bb520e9686a
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-15 17:59:20 -0600

    Featued component created
    Basic skelleton of the featued component to be used on the carrusel

---
+++ src/components/featured/Featured.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Carrusel from "./Carrusel";
+
+const Featured = () => {
+  return (
+    <div style={{ postion: "relative" }}>
+      <div className="artist_name">
+        <div className="wrapper">Ariana Grande</div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
