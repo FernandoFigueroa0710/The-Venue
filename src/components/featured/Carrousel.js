ad6ba9003062f9bec97adc8a94b860beb1cd96b4
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-16 00:12:17 -0600

    add additional slides to the app

--- src/components/featured/Carrousel.js
+++ src/components/featured/Carrousel.js
@@ -4,7 +4,8 @@ import Slider from "react-slick";
 import slide_one from "../../resources/images/slide_one.jpg";
 import slide_two from "../../resources/images/slide_two.jpg";
 import slide_three from "../../resources/images/slide_three.jpg";
-
+import slide_four from "../../resources/images/slide_four.jpg";
+import slide_five from "../../resources/images/slide_five.jpg";
 const Carrousel = () => {
   const settings = {
     dots: false,
@@ -45,6 +46,24 @@ const Carrousel = () => {
             className="carrousel_image"
             style={{
               background: `url(${slide_three})`,
+              height: `${window.innerHeight}px`
+            }}
+          />
+        </div>
+        <div>
+          <div
+            className="carrousel_image"
+            style={{
+              background: `url(${slide_four})`,
+              height: `${window.innerHeight}px`
+            }}
+          />
+        </div>
+        <div>
+          <div
+            className="carrousel_image"
+            style={{
+              background: `url(${slide_five})`,
               height: `${window.innerHeight}px`
             }}
           />
