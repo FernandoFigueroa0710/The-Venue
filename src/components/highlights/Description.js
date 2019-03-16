341a3dc93139dc507641a4016cdf56c07b3b70b1
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-15 22:19:42 -0600

    description component

---
+++ src/components/highlights/Description.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Fade from "react-reveal/Fade";
+
+const Description = () => {
+  return (
+    <Fade top duration={500}>
+      <div className="center_wrapper">
+        <h2>Highlights of the Event</h2>
+        <div className="highlight_description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna sit
+          amet purus gravida quis blandit. Dui vivamus arcu felis bibendum ut
+          tristique et egestas. Consequat id porta nibh venenatis cras sed felis
+          eget. Faucibus in ornare quam viverra. At quis risus sed vulputate
+          odio ut enim blandit. Sit amet venenatis urna cursus eget. Neque
+          egestas congue quisque egestas diam in arcu cursus. Risus nullam eget
+          felis eget nunc lobortis mattis. Nisl nisi scelerisque eu ultrices
+          vitae auctor eu augue. Aenean pharetra magna ac placerat vestibulum
+          lectus mauris. Sit amet massa vitae tortor condimentum lacinia quis
+          vel eros. Varius morbi enim nunc faucibus a pellentesque sit. Orci
+          porta non pulvinar neque laoreet. Volutpat diam ut venenatis tellus in
+          metus vulputate. Mi ipsum faucibus vitae aliquet nec ullamcorper.
+          Varius quam quisque id diam vel quam. Molestie at elementum eu
+          facilisis sed odio morbi quis commodo. Adipiscing commodo elit at
+          imperdiet. Hac habitasse platea dictumst vestibulum rhoncus est
+          pellentesque elit.
+        </div>
+      </div>
+    </Fade>
+  );
+};
+
+export default Description;
