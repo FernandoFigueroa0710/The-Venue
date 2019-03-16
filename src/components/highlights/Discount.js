016e88e9bdd8d4894f9b5426c50a6f01b9fe86b0
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-15 23:04:52 -0600

    add function to increase discount percentage

--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -3,13 +3,32 @@ import Fade from "react-reveal/Fade";
 import Slide from "react-reveal/Slide";
 
 class Discount extends Component {
+  state = {
+    discountStart: 0,
+    discountEnd: 30
+  };
+
+  percentage = () => {
+    if (this.state.discountStart < this.state.discountEnd) {
+      this.setState({
+        discountStart: this.state.discountStart + 1
+      });
+    }
+  };
+
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.percentage();
+    }, 50);
+  }
+
   render() {
     return (
       <div className="center_wrapper">
         <div className="discount_wrapper">
-          <Fade left>
+          <Fade left onReveal={() => this.percentage()}>
             <div className="discount_percentage">
-              <span>23%</span>
+              <span>{this.state.discountStart}%</span>
               <span>OFF</span>
             </div>
           </Fade>
