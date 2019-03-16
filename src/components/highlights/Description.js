c905003486f6e9873a3fb52c9d08a34fe7b42b1e
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-15 22:26:23 -0600

    eliminated unnesesary text

--- src/components/highlights/Description.js
+++ src/components/highlights/Description.js
@@ -12,18 +12,7 @@ const Description = () => {
           amet purus gravida quis blandit. Dui vivamus arcu felis bibendum ut
           tristique et egestas. Consequat id porta nibh venenatis cras sed felis
           eget. Faucibus in ornare quam viverra. At quis risus sed vulputate
-          odio ut enim blandit. Sit amet venenatis urna cursus eget. Neque
-          egestas congue quisque egestas diam in arcu cursus. Risus nullam eget
-          felis eget nunc lobortis mattis. Nisl nisi scelerisque eu ultrices
-          vitae auctor eu augue. Aenean pharetra magna ac placerat vestibulum
-          lectus mauris. Sit amet massa vitae tortor condimentum lacinia quis
-          vel eros. Varius morbi enim nunc faucibus a pellentesque sit. Orci
-          porta non pulvinar neque laoreet. Volutpat diam ut venenatis tellus in
-          metus vulputate. Mi ipsum faucibus vitae aliquet nec ullamcorper.
-          Varius quam quisque id diam vel quam. Molestie at elementum eu
-          facilisis sed odio morbi quis commodo. Adipiscing commodo elit at
-          imperdiet. Hac habitasse platea dictumst vestibulum rhoncus est
-          pellentesque elit.
+          odio ut enim blandit. Sit amet venenatis urna cursus eget.
         </div>
       </div>
     </Fade>
