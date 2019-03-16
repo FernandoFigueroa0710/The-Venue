2518157d01489b74484750315635392e7f0d92cf
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-15 18:01:58 -0600

    Featured component added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./resources/styles.css";
 
 import Header from "../src/components/header_footer/Header";
+import Featured from "../src/components/featured/Featured";
 
 class App extends Component {
   render() {
     return (
       <div style={{ height: "1500px", background: "grey" }}>
         <Header />
+        <Featured />
       </div>
     );
   }
